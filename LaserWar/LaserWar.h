a4f4754f9a9e9086ce4c77bae8715e955156db59
“Eugeniy <eugeniy@fivetran.com>
2023-02-18 19:29:40 +0100

    Implement send command for a phototransistor

--- LaserWar/LaserWar.h
+++ LaserWar/LaserWar.h
@@ -1,25 +1,141 @@
 /*
-  LaserWar.h - Library for flashing Morse code.
+  LaserWar.h
   Commands for sending, recieving and decoding LaserWar v9 commands
   Created by Eugeniy Maslyaev (mrjohn.ru), 
-  September 9, 2019.
+  February 19, 2023.
 */
+
+#define CARRIER_INTERVAL 4 // for DigiSpark
+//#define CARRIER_INTERVAL 6 // for Arduino Pro Mini / Nano
+#define MLT_SPACE_INTERVAL 600
+#define MLT_HEADER_CYCLES 128 // adjusted experimentally
+#define MLT_BIT0_CYCLES 32 // adjusted experimentally
+#define MLT_BIT1_CYCLES 64 // adjusted experimentally
+
+#define HEADING_RECEIVED 2
 
 #ifndef LaserWar_h
 #define LaserWar_h
-#include "Arduino.h"
+#include <Arduino.h>
 
 class LaserWar {
-	public:
-		LaserWar();
-		unsigned long waitCommand(byte pin);
-		void send(byte pin, unsigned long cmd);
 	private:
-		void sendPulse(int cycles, byte pin);
-		void sendByte(byte x, byte pin);
-		void sendHeader(byte pin);
-		int decodePulse(byte pin);
-		bool isCorrect(unsigned long duration, int etalon);
+		uint8_t pin;
+		unsigned long lostSignalTime, getSignalTime, buffer;
+		unsigned int thresholod, headerLength, oneLength, calmValue;
+		bool receivingSignal;
+
+		void sendPulse(int cycles) {
+			for (int i=0; i<cycles; i++ ) {
+				digitalWrite(this->pin, HIGH);
+				delayMicroseconds(CARRIER_INTERVAL);
+				digitalWrite(this->pin, LOW);
+				delayMicroseconds(CARRIER_INTERVAL);
+			}
+		}
+		
+		void sendHeader() {
+			this->sendPulse(MLT_HEADER_CYCLES);
+			delayMicroseconds(MLT_SPACE_INTERVAL);
+		}
+
+		bool hasSignal(int result){
+			return result < this->calmValue;
+		}
+
+		short decodeSignal(unsigned long duration){
+			if (duration < this->thresholod){
+				return -1;
+			}
+
+			this->getSignalTime = 0;
+
+			if (duration >= this->headerLength) {
+				return HEADING_RECEIVED;
+			}
+
+			if (duration >= this->oneLength) {
+				return 1;
+			}
+
+			return 0;
+		}
+
+		void sendByte(byte x) {
+			for(int i=0; i<8; i++) {
+				if (x & 0x80) {
+				this->sendPulse(MLT_BIT1_CYCLES);
+				} else {
+				this->sendPulse(MLT_BIT0_CYCLES);
+				}
+				x = x << 1;
+				delayMicroseconds(MLT_SPACE_INTERVAL);
+			}
+		}
+	public:
+		void setPin(uint8_t pin){ this->pin = pin; }
+		void setThreshold(unsigned int threshold){ this->thresholod = threshold; }
+		void setDurationOfHeader(unsigned int headerLength){ this->headerLength = headerLength; }
+		void setDurationOfOne(unsigned int oneLength){ this->oneLength = oneLength; }
+		void setReceiverCalmValue(unsigned int calmValue){ this->calmValue = calmValue; }
+
+		LaserWar(uint8_t pin) {
+			this->setPin(pin);
+			this->buffer = 0;
+			this->getSignalTime = 0;
+			this->lostSignalTime = 0;
+			this->receivingSignal = false;
+			this->thresholod = 50;
+			this->headerLength = 2000;
+			this->oneLength = 1000;
+			this->calmValue = 995;
+		}
+
+		void send(unsigned long cmd){
+			sendHeader();
+			sendByte(cmd >> 16);
+			sendByte((cmd & 0x00ffff) >> 8);
+			sendByte(cmd & 0x0000ff);
+		}
+
+		unsigned long read(){
+			int result = analogRead(this->pin);
+			unsigned long time = micros();
+			
+			if (this->hasSignal(result)){
+				if (!this->receivingSignal){
+				if (time - this->lostSignalTime > this->thresholod){
+					this->receivingSignal = true;
+					this->getSignalTime = time;
+					this->lostSignalTime = 0;
+				}
+				} else {
+					this->lostSignalTime = 0;
+				}
+			} else {
+				if (this->receivingSignal){
+					if (time - this->lostSignalTime > this->thresholod && this->lostSignalTime > 0){
+						this->receivingSignal = false;
+						short signal = decodeSignal(this->lostSignalTime - this->getSignalTime);
+						if (signal >= 0){
+							if (signal == HEADING_RECEIVED){
+								this->buffer = 0;
+							} else {
+								this->buffer = (this->buffer << 1) + signal;
+							}
+						}
+					}
+					this->lostSignalTime = time;
+				} else {
+					if (this->lostSignalTime > 0 && time - this->lostSignalTime > this->headerLength){
+						this->lostSignalTime = 0;
+						return this->buffer;
+					}
+				}
+			}
+
+			return 0;
+		}
 };
 
 #endif
--- LaserWar/examples/read_command/read_command.ino
+++ LaserWar/examples/read_command/read_command.ino
@@ -1,20 +1,25 @@
 #include <Arduino.h>
 #include <LaserWar.h>
 
-// Use TSOP 34856 to decode signals
-#define TSOP_34856_PIN 3
+/*
+Используем фототранзистор 940нм и подключаем с общим эмиттером.
+Подключаем короткую ногу (коллектор) через резистор 1 КОм (или 10 КОм) к +5В
+Эту же кототкую ногу (коллектор) подключаем к аналоговому пину, например, A0, напрямую
+Длинную ногу (эмиттер) подключаем к GND
+*/
+#define RECEIVER_PIN A0
 
-LaserWar lw;
+LaserWar lw(RECEIVER_PIN);
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Ready");
-  pinMode(TSOP_34856_PIN, INPUT);
 }
 
 void loop() {
-  unsigned long cmd = lw.waitCommand(TSOP_34856_PIN);
+  // Вернет 0, если команда не была считана или команду, если она успешно считалась
+  unsigned long cmd = lw.read();
   if (cmd) {
-    Serial.println(cmd);
+    Serial.println(cmd, HEX);
   }
 }

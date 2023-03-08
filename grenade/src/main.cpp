363e994444c2d558d921e6a0aedd881b9123d1a6
“Eugeniy <eugeniy@fivetran.com>
2023-03-08 23:06:13 +0100

    Update laserwar library and add timers

--- grenade/src/main.cpp
+++ grenade/src/main.cpp
@@ -1,26 +1,33 @@
 #include <Arduino.h>
-#include <LaserWar.h>
 #include <EEPROMex.h>
+#include <LaserWar.h>
+#include <LWCommand.h>
+#include <EncButton.h>
+#include <TimerMs.h>
 
 #define TONE_PIN 0
 #define BTN_PIN 1
-#define RESET_PIN 2
+#define TSOP_PIN 2
 #define IR_PIN 3
 #define BANGS_COUNT 3
 #define BEFORE_BANG_DELAY 4
-#define RESPAWN_CMD 0x8305e8
-#define BANG_CMD 0x830be8
 #define SETTINGS_WAIT_TIME 5000
-#define MINUTES_BEFORE_SEARCH 15
+#define MINUTES_BEFORE_SEARCH 30
 
 unsigned char used = '0';
 #define RESPAWN_MODES_COUNT 5
 unsigned long respawnTimes[RESPAWN_MODES_COUNT] = { 0, 5000, 60000, 300000, 600000 };
-unsigned long lastUsedTime = 0;
-unsigned long searchStartTime = 0;
 byte respawnMode = 2;
 
-LaserWar lw;
+LaserWar lwSender(IR_PIN);
+LaserWar lwReceiver(TSOP_PIN);
+LWCommand bangCmd(LwSetting::AdminCommand, LwAdminSetting::Explode);
+LWCommand respawnCmd;
+
+EncButton<EB_TICK, BTN_PIN> btn;
+TimerMs settingsTimer(SETTINGS_WAIT_TIME, true, true);
+TimerMs searchTimer(MINUTES_BEFORE_SEARCH * 60000, false, true);
+TimerMs respawnTimer(respawnTimes[respawnMode], false, true);
 
 void beep(int duration, byte count){
     Serial.print("BEEP: "); Serial.print(duration); Serial.print("; "); Serial.println(count);
@@ -38,8 +45,8 @@ void beep(int duration, byte count){
 void restore(){
   beep(700, 1);
   used = '0';
-  lastUsedTime = 0;
-  searchStartTime = millis();
+  respawnTimer.stop();
+  searchTimer.start();
   Serial.println("Restore!");
   EEPROM.write(0, used);
 }
@@ -50,12 +57,15 @@ void bang(){
   delay(500);
 
   for (byte i = 0; i < BANGS_COUNT; i++){
-    lw.send(IR_PIN, BANG_CMD);
+    lwSender.send(bangCmd);
     delay(100);
   }
 
   used = '1';
-  searchStartTime = 0;
+  searchTimer.start();
+  if (respawnMode != 0) {
+    respawnTimer.start();
+  }
   EEPROM.write(0, used);
 
   beep(2000, 1);
@@ -78,6 +88,10 @@ void loadSettings(){
         saveSettings();
     } else {
         respawnMode = EEPROM.read(1);
+        if (respawnMode != 0) { 
+            respawnTimer.setTime(respawnTimes[respawnMode]);
+            respawnTimer.start(); 
+        }
     }
     Serial.print("Loaded settings: ");
     Serial.print("Respawn Mode = ");
@@ -88,9 +102,10 @@ void loadSettings(){
 
 void setup() {
   pinMode(IR_PIN, OUTPUT);
-  pinMode(RESET_PIN, INPUT);
+  pinMode(TSOP_PIN, INPUT);
   pinMode(BTN_PIN, INPUT);
   pinMode(TONE_PIN, OUTPUT);
+  btn.setButtonLevel(HIGH);
   Serial.begin(9600);
   loadSettings();
 }
@@ -116,79 +131,59 @@ void nextMode(){
 void settingsMode(){
     Serial.println("Settings mode");
     beep(200, 3);
-    unsigned long keyDownTime = 0;
     while (true){
-        unsigned long t = millis();
-        if (digitalRead(BTN_PIN) == HIGH){
-            if (keyDownTime == 0){
-                keyDownTime = t;
-            } else if (t - keyDownTime > 1000){
-                keyDownTime = 0;
-                return;
-            }
-        } else if (keyDownTime > 0) {
-            if (keyDownTime > 0 && t - keyDownTime < 1000){
-                keyDownTime = 0;
-                nextMode();
-            }
+        btn.tick();
+        if (btn.click()) { nextMode(); }
+        if (btn.held()) { 
+            saveSettings();
+            beep(200, 4);
+            return; 
         }
     }
 }
 
 void searchMode(){
+    searchTimer.setTime(200);
+    searchTimer.setPeriodMode();
+    searchTimer.start();
+    bool state = true;
+
     while (true){
-        if (digitalRead(BTN_PIN) == HIGH){
-            searchStartTime = 0;
-            delay(2000);
+        btn.tick();
+        if (searchTimer.tick()){
+            state ? tone(TONE_PIN, 1000) : noTone(TONE_PIN);
+            state = !state;
+        }
+
+        if (btn.click()){
+            searchTimer.setTime(MINUTES_BEFORE_SEARCH * 60000);
+            searchTimer.setTimerMode();
+            searchTimer.start();
+            beep(100, 1);
             return;
         }
-        beep(100, 1);
-        delay(200);
     }
 }
 
-unsigned long keyDownTime = 0;
 void loop() {
-    unsigned long t = millis();
-    if (t < SETTINGS_WAIT_TIME){
-        if (digitalRead(BTN_PIN) == HIGH){
-          if (keyDownTime == 0){
-              keyDownTime = t;
-          } else if (t - keyDownTime >= 1000){
-              keyDownTime = 0;
-              settingsMode();
-              saveSettings();
-              lastUsedTime = 0;
-              beep(200, 4);
-          }
-        } else {
-          if (keyDownTime > 0 && used == '0' && t - keyDownTime < 1000){
-            keyDownTime = 0;
-            bang();
-          }
-        }
+    btn.tick();
+    settingsTimer.tick();
+
+    if (settingsTimer.active() && btn.press()){ settingsMode(); }
+    if (searchTimer.tick()){ searchMode(); delay(2000); }
+
+    if (used == '0') {
+        if (btn.click()) { bang(); }
     } else {  
-      if (used == '1'){
-          if (respawnMode == 0){
-              unsigned long cmd = lw.waitCommand(RESET_PIN);
-              if (cmd == RESPAWN_CMD){
-                restore();
-                searchStartTime = 0;
-              }
-          } else {
-              if (lastUsedTime == 0) lastUsedTime = t;
-              if (t - lastUsedTime >= respawnTimes[respawnMode]){
-                  restore();
-              }
-          }
-      } else {
-          if (searchStartTime > 0 && t - searchStartTime > MINUTES_BEFORE_SEARCH * 60000){
-            searchMode();
-          } else {
-            if (digitalRead(BTN_PIN) == HIGH){
-                bang();
+        if (respawnMode == 0){
+            unsigned long cmd = lwReceiver.read();
+            if (respawnCmd.load(cmd)){
+                if (respawnCmd.getCommand() == LWCommand(LwSetting::AdminCommand, LwAdminSetting::NewGame).getCommand()){
+                    restore();
+                }
             }
-          }
-      }
+        } else {
+            if (respawnTimer.tick()) { restore(); }
+        }
     }
 }
